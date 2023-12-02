0f36df3a7043d71e08e45e2729f2b2098fdc13d6
aamir <16462830+batakpout@users.noreply.github.com>
2023-12-02 23:03:33 +0530

    Create Problem2.scala

---
+++ dsalgo_lab/leetcode/Problem2.scala
@@ -0,0 +1,52 @@
+package dsalgo_lab.leetcode
+
+object Problem2 extends App {
+
+
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+  }
+
+  def show(head: ListNode): Unit = {
+    var currentNode = head
+    while (currentNode != null) {
+      print(currentNode.x + " -> ")
+      currentNode = currentNode.next
+    }
+    print("null")
+  }
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    var list1 = l1
+    var list2 = l2
+    var carry = 0
+    val dummyNode = new ListNode(0)
+    var currentNode = dummyNode
+
+    while (list1 != null || list2 != null || carry > 0) {
+
+      if (list1 != null) {
+        carry = carry + list1.x
+        list1 = list1.next
+      }
+
+      if (list2 != null) {
+        carry = carry + list2.x
+        list2 = list2.next
+      }
+      val node = new ListNode(carry % 10)
+      carry = carry / 10
+      currentNode.next = node
+      currentNode = node
+    }
+
+    dummyNode.next
+  }
+
+  val l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))
+  val l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+  val res = addTwoNumbers(l1, l2)
+  show(res)
+
+}
