60f3d82e2b0d3cf03b828236e11a3226f7d646f2
aamir <16462830+batakpout@users.noreply.github.com>
2023-12-13 14:33:05 +0530

    Create Patterns.scala

---
+++ dsalgo_lab/patterns/Patterns.scala
@@ -0,0 +1,136 @@
+package dsalgo_lab.patterns
+
+/**
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ */
+object Pattern1 extends App {
+
+  def printPattern(n: Int): Unit = {
+    for (i <- 1 to n) {
+      for (_ <- 1 to i) {
+        print("*\t")
+      }
+      println()
+    }
+  }
+
+  printPattern(10)
+}
+
+/**
+  * * * * *
+  * * * *
+  * * *
+  * *
+  *
+ */
+object Pattern2 extends App {
+
+  def printPattern(n: Int): Unit = {
+    for (i <- 1 to n) {
+      for (_ <- i to n) {
+        print("*\t")
+      }
+      println()
+    }
+  }
+
+  printPattern(10)
+}
+
+/**
+            *
+         *  *
+      *  *  *
+   *  *  *  *
+ * *  *  *  *
+ */
+
+object Patterns3 extends App {
+
+   def printPatterns(n: Int): Unit = {
+     var sp = n - 1
+     var st = 1
+     for (_ <- 1 to n) {
+
+         for(_ <- 1 to sp) {
+            print("\t")
+         }
+
+        for (_ <- 1 to st) {
+         print("*\t")
+       }
+            sp -= 1
+            st += 1
+            println()
+
+     }
+   }
+  printPatterns(10)
+}
+
+/**
+   * * * * *
+     * * * *
+        * * *
+          * *
+            *
+ */
+
+object Patterns4 extends App {
+
+  def printPatterns(n: Int): Unit = {
+    var sp = 0
+    var st = n
+    for (_ <- 1 to n) {
+
+      for(_ <- 1 to sp) {
+        print("\t")
+      }
+
+      for (_ <- 1 to st) {
+        print("*\t")
+      }
+      sp += 1
+      st -= 1
+      println()
+
+    }
+  }
+  printPatterns(10)
+}
+
+/**
+         *
+      *  *  *
+   *  *  *  *  *
+      *  *  *
+         *
+ */
+object Patterns5 extends App {
+
+  def printPatterns(n: Int): Unit = {
+    //make changes to it 
+    var sp = 0
+    var st = n
+    for (_ <- 1 to n) {
+
+      for(_ <- 1 to sp) {
+        print("\t")
+      }
+
+      for (_ <- 1 to st) {
+        print("*\t")
+      }
+      sp += 1
+      st -= 1
+      println()
+
+    }
+  }
+  printPatterns(10)
+}
