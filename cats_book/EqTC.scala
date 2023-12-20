2ad175863cc1014bbee53e76d892304e91f53d7e
aamir <16462830+batakpout@users.noreply.github.com>
2023-12-20 20:20:16 +0530

    Update EqTC.scala

--- cats_book/EqTC.scala
+++ cats_book/EqTC.scala
@@ -71,7 +71,7 @@ object EqTC extends App {
   case class Cat(name: String, age: Int, color: String)
 
   implicit val eqCat: Eq[Cat] = Eq.instance[Cat]{ (c1, c2) =>
-    c1.name === c2.name && c1.age == c2.age && c1.color == c2.color
+    (c1.name === c2.name) && (c1.age == c2.age) && (c1.color == c2.color)
   }
 
   val c1 = Cat("pussy", 2, "white")
