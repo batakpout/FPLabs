868438079f76bcbcb8400a6fd434a459c29cc93d
aamir <16462830+batakpout@users.noreply.github.com>
2023-12-28 13:22:45 +0530

    Create Problem541.scala

---
+++ dsalgo_lab/arrays/Problem541.scala
@@ -0,0 +1,15 @@
+package dsalgo_lab.leetcode
+
+object Problem541FP extends App {
+
+
+  val k = 2
+  val str = "abcd"
+
+  val result = str.grouped(k * 2).map(s => s.take(k).reverse ++ s.drop(k)).toList.mkString
+  println(result)
+}
+
+object Problem541FasterVersion extends App {
+  
+}
