0617bcdcacd1e05440243730fe00f5c1d4fac29c
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-06 10:55:19 +0530

    Update NewtonMethodSquareRoot.scala

--- dsalgo_lab/math/NewtonMethodSquareRoot.scala
+++ dsalgo_lab/math/NewtonMethodSquareRoot.scala
@@ -1,7 +1,7 @@
 package dsalgo_lab.math
 
 object NewtonSquareRoot extends App {
-   /**
+  /**
    * The classical way to achieve this is by successive approximations using Newton’s method
    * x(n) = x - f(x)/f(x')
    * e.g x = squareroot(N) -> x * x - n = 0
@@ -10,19 +10,20 @@ object NewtonSquareRoot extends App {
    */
   def abs(x: Double): Double = if (x < 0) -x else x
 
-  def improve(guess: Double, x: Double): Double = (guess + x / guess) / 2
+  def improve(guess: Double, N: Double): Double = (guess + N / guess) / 2
 
-  def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) < 0.001
+  def isGoodEnough(guess: Double, N: Double) = abs(guess * guess - N) < 0.001
 
   //def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) / 2 < 0.001 -- fit for large +ive & -ive numbers
-  def sqrItr(guess: Double, x: Double): Double = {
-    if (isGoodEnough(guess, x)) guess else sqrItr(improve(guess, x), x)
+  def sqrItr(guess: Double, N: Double): Double = {
+    if (isGoodEnough(guess, N)) guess else sqrItr(improve(guess, N), N)
   }
 
   def squareRoot(n: Int) = sqrItr(1.0, n)
 
   def isPerfectSquare(number: Int): Boolean = {
     val sqrtValue = squareRoot(number) //Math.sqrt(number)
+    println(s" number: $number, sqrt: $sqrtValue, trunc: ${sqrtValue.toInt}")
     val truncatedSqrt = sqrtValue.toInt
     truncatedSqrt * truncatedSqrt == number
   }
