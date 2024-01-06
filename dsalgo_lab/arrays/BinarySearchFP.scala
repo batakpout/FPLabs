33377b96e0a6a01816aa14d41af80fa6c0ede048
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-06 16:37:45 +0530

    Update BinarySearchFP.scala

--- dsalgo_lab/arrays/BinarySearchFP.scala
+++ dsalgo_lab/arrays/BinarySearchFP.scala
@@ -3,16 +3,17 @@ package dsalgo_lab.arrays
 object BinarySearchImperative extends App {
 
   // TC: O(LogN)
-  def search(arr: Array[Int], target: Int, start: Int, end: Int): Int = {
-    if (start > end) -1 else {
-      val mid = start + ((end - start) / 2)
-      arr(mid) match {
-        case i if i == target => mid
-        case i if i > target => search(arr, target, start, mid - 1)
-        case i if i < target => search(arr, target, mid + 1, end)
-      }
-    }
+  def search(arr: Array[Int], target: Int): Int = {
+     def rec(start: Int = 0, end: Int = arr.length):Int = {
+       if(start <= end) {
+         val mid = start + ((end - start) / 2)
+         if(arr(mid) == target) mid
+         else if(arr(mid) > target) rec(start, end - 1)
+         else rec(start + 1, end)
+       } else -1
+     }
+    rec()
   }
 
-  println(search(Array(1, 2, 3, 4, 5, 6, 7, 8), 8, 0, 7))
+  println(search(Array(1, 2, 3, 4, 5, 6, 7, 8), 8))
 }
