cad75dcc3b03f6beeee386b3f3180272710b1c27
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-08 23:39:15 +0530

    Update SubArrays.scala

--- dsalgo_lab/arrays/SubArrays.scala
+++ dsalgo_lab/arrays/SubArrays.scala
@@ -6,6 +6,7 @@ object SubArrays extends App {
    The third loop (innermost) does not contribute to a significant change in the overall time complexity, 
    as it is dominated by the first two loops.
    */
+ //todo: find an optimal solution?
     def subArrays(a: Array[Char]): Unit = {
        for(i <- 0 until a.length) {
          for(j <- i until a.length) {
