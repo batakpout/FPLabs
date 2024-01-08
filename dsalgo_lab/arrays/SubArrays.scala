70f10d20c8b339118fabb6206c28407d51c52391
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-08 23:10:15 +0530

    Update SubArrays.scala

--- dsalgo_lab/arrays/SubArrays.scala
+++ dsalgo_lab/arrays/SubArrays.scala
@@ -1,7 +1,11 @@
 package dsalgo_lab.arrays
 
 object SubArrays extends App {
-
+ //TC: O(N^2 or N^3))
+  /**
+   The third loop (innermost) does not contribute to a significant change in the overall time complexity, 
+   as it is dominated by the first two loops.
+   */
     def subArrays(a: Array[Char]): Unit = {
        for(i <- 0 until a.length) {
          for(j <- i until a.length) {
