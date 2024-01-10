289caa187d778060c8f8838cb0394c2a635e1e91
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-10 22:55:43 +0530

    Update Monad.scala

--- cats_book/Monad.scala
+++ cats_book/Monad.scala
@@ -1,1 +1,28 @@
 
+package cats_book
+
+object MonadExample1 extends App {
+
+  def parseInt(a: String): Option[Int] =
+    a.toIntOption
+
+  def divide(a: Int, b: Int): Option[Int] =
+    if(b == 0) None else Some(a / b)
+
+  def stringDivideBy(s1: String, s2: String): Option[Int] =
+    parseInt(s1).flatMap { i1 =>
+      parseInt(s2).flatMap { i2 =>
+        divide(i1, i2)
+      }
+    }
+
+   println(stringDivideBy("10", "2"))
+   println(stringDivideBy("10", "aa"))
+
+  def stringDivideByFC(s1: String, s2: String): Option[Int] =
+    for {
+      i1 <- parseInt(s1)
+      i2 <- parseInt(s2)
+      r <- divide(i1, i2)
+    } yield r
+}
