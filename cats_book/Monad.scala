877aedac745d216435f630ff56ef27712f1a7e24
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-11 21:11:45 +0530

    Update Monad.scala

--- cats_book/Monad.scala
+++ cats_book/Monad.scala
@@ -1,6 +1,4 @@
-
 package cats_book
-
 object MonadExample1 extends App {
 
   def parseInt(a: String): Option[Int] =
@@ -16,8 +14,8 @@ object MonadExample1 extends App {
       }
     }
 
-   println(stringDivideBy("10", "2"))
-   println(stringDivideBy("10", "aa"))
+  println(stringDivideBy("10", "2"))
+  println(stringDivideBy("10", "aa"))
 
   def stringDivideByFC(s1: String, s2: String): Option[Int] =
     for {
@@ -25,4 +23,41 @@ object MonadExample1 extends App {
       i2 <- parseInt(s2)
       r <- divide(i1, i2)
     } yield r
+}
+object Monads extends App {
+
+   trait Monad_[F[_]] {
+
+     def pure[A](a: A): F[A]
+     def flatMap[A, B](fa: F[A])(f: A => F[B]):F[B]
+     def map[A, B](fa: F[A])(f: A => B):F[B] =
+       flatMap(fa)(a => pure(f(a)))
+       
+     def ap[A, B](f: F[A => B])(fa: F[A]): F[B]
+     
+     //def map2[A, B](fa: F[A])(f: A => B) = {
+      // val f = ap(pure(f)) -- to be continued check ap method impl. in Apply , applicative, FlatMap
+     }
+
+     /**
+       Laws:
+       1. left identity: pure(a).flatMap(f) == f(a)
+       2. right identity: m.flatMap(pure) == m
+       3. associativity: m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
+      */
+
+      /**
+        map method of Option:
+        final def map[B](f: A => B): Option[B] =
+                         if (isEmpty) None else Some(f(this.get))
+
+        def flatMap[B](f: A => Option[B]): Option[B] =
+          if(isEmpty) None else f(this.get)
+
+       */
+   }
+
+
+
+
 }
