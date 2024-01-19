21183b1c85e202d570042e7781b99f858bc3ac11
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-19 07:15:44 +0530

    Update EitherMonad.scala

--- cats_book/EitherMonad.scala
+++ cats_book/EitherMonad.scala
@@ -2,8 +2,7 @@ package cats_book
 
 object EitherMonad1 extends App {
 
-  //In addition to creating instances of Right, Left directly
-  // we can also import asRight and asLeft extension methods from cats.syntax.either
+
   val either1: Right[Nothing, Int] = Right(10)
   val either2: Either[String, Int] = Right(30)
   //val either2: Right[  , Int] = Right(30) ; actual type inferred by compiler
@@ -16,32 +15,38 @@ object EitherMonad1 extends App {
 
   import cats.syntax.either._
 
+  //In addition to creating instances of Right, Left directly
+  // we can also import asRight and asLeft extension methods from cats.syntax.either
+
   val a1: Either[String, Int] = 3.asRight[String]
   val b1 = 4.asRight[String]
 
   val r1: Right[Nothing, Int] = Right(0)
   val r2: Either[String, Int] = r1.map(_ + 1)
- /* def countPositive(nums: List[Int]) =
-    nums.foldLeft(Right(0)){ (acc, num) =>
-      if(num > 0) acc.map(_ + 1)
-      else Left("Negative Stopping!")
-    }*/
+
+  /* def countPositive(nums: List[Int]) =
+     nums.foldLeft(Right(0)){ (acc, num) =>
+       if(num > 0) acc.map(_ + 1)
+       else Left("Negative Stopping!")
+     }*/
   // they help avoid type inference problems caused by over-narrowing
- //override def foldLeft[B](z: B)(op: (B, A) => B): B = {
+  //override def foldLeft[B](z: B)(op: (B, A) => B): B = {
   def countPositive(nums: List[Int]) =
     nums.foldLeft(0.asRight[String]) { (acc, num) =>
       if (num > 0) acc.map(_ + 1)
       else Left("Negative Stopping!")
     }
 
-   println(countPositive(List(1,2,3,4,5)))
-   println(countPositive(List(1,-2)))
+  println(countPositive(List(1, 2, 3, 4, 5)))
+  println(countPositive(List(1, -2)))
 }
 
 object EitherMonad2 extends App {
 
   //cat.syntax.either add some useful extension methods to Either companion object
-   import cats.syntax.either._
+
+  import cats.syntax.either._
+
   val e1: Either[NumberFormatException, Int] = Either.catchOnly[NumberFormatException]("foo".toInt)
   val e2: Either[NumberFormatException, Int] = Either.catchOnly[NumberFormatException]("12".toInt)
 
@@ -71,8 +76,10 @@ object EitherMonad2 extends App {
   val e9: Int = "Error".asLeft[Int].getOrElse(12) // 12
   val e10: Int = 10.asRight[String].getOrElse(22) // 10
 
+  //getOrElse , orElse to extract values  from the right side or return a default
   val e11: Either[String, Int] = "Error".asLeft[Int].orElse(2.asRight)
 
+  //check whether right hand side satisfies a predicate
   val e12 = -1.asRight[String].ensure("Must be non-negative!")(_ > 0)
   val e13 = 21.asRight[String].ensure("Must be non-negative!")(_ > 0)
 
@@ -97,6 +104,7 @@ object EitherMonad2 extends App {
 
   println("###############")
 
+  // there are leftMap and bimap methods to complement map
   val e16: Either[String, Int] = 1.asRight[String].leftMap(_.reverse)
   val e17: Either[String, Int] = "oof".asLeft[Int].leftMap(_.reverse)
 
@@ -108,5 +116,57 @@ object EitherMonad2 extends App {
   println(e18)
   println(e19)
 
+  val e20: Either[String, Int] = 123.asRight[String]
+  val e21: Either[Int, String] = e20.swap
 
+  val e22: Either[String, Int] = "error".asLeft[Int]
+  val e23: Either[Int, String] = e22.swap
+
+  println(e20)
+  println(e21)
+  println(e22)
+  println(e23)
+
+
+}
+
+object EitherErrorHandling1 extends App {
+  //either is typically used to implement fail-fast error handling.
+
+  import cats.syntax.either._
+  type Result[A] = Either[String, A]
+
+  val e1: Result[Int] = for {
+    a <- 6.asRight[String]
+    b <- 0.asRight[String]
+    c <- if (b == 0) "DIV0".asLeft[Int] else (a / b).asRight[String]
+  } yield c * 10
+
+  println(e1)
+}
+
+object EitherAlgebricDataTypeErrorHandling extends App {
+   object wrapper {
+     sealed trait LoginError extends Product with Serializable
+     final case class UserNotFound(userName: String) extends LoginError
+     final case class PasswordIncorrect(userName: String) extends LoginError
+     case object UnexpectedError extends LoginError
+   }
+  import wrapper._
+   case class User(userName: String, password: String)
+   type LoginResult = Either[LoginError, User]
+
+  //choose error handling behaviour based on type:
+  def handleError(error: LoginError): Unit = error match {
+    case UserNotFound(u) => println(s"User not found $u")
+    case PasswordIncorrect(u) => println(s"Password Incorrect: $u")
+    case UnexpectedError => println("unexpected error")
+  }
+  import cats.syntax.either._
+
+  val result1 = User("dave", "1234").asRight // === User("dave", "1234").asRight[LoginError]
+  val result2: LoginResult = UserNotFound("dave").asLeft // === UserNotFound("dave").asLeft[User]
+
+  result1.fold(handleError, println) // User("dave", "1234")
+  result2.fold(handleError, println) // User dave not found
 }
