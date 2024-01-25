46fdbe94d8d3ede2a56d02276b303bbb733618e0
aamir <16462830+batakpout@users.noreply.github.com>
2024-01-25 16:15:59 +0530

    Create KleisliType.scala

---
+++ cats_lab/KleisliType.scala
@@ -0,0 +1,48 @@
+package cats_labs
+
+import cats.data.Kleisli
+
+/**
+ Kleisli enables composition of functions that return a monadic value
+ Benefits:
+ * Allows programming in a more composition like style.
+ * Abstracts away the lifting of values into a Monad.
+ */
+object KelisliType1 extends App {
+
+  // case 1
+  val parse: String => Option[Int] =
+    s => if (s.matches("-?[0-9]+")) Some(s.toInt) else None
+
+  val reciprocal: Int => Option[Double] =
+    i => if (i != 0) Some(12.0 / i) else None
+
+
+
+  //FUNCTION COMPOSITION
+
+  /**
+   * This is where the problem starts. If you want to compose both functions, you cannot use compose
+   * or even do it as a variable like the original ones. Instead, you have to declare a function and
+   * start nesting flatMaps, which really harms readability.
+   */
+  def parseAndReciprocal(s: String): Option[Double] = parse(s).flatMap(reciprocal)
+
+  println(parseAndReciprocal("6"))
+
+
+  //USING KLEISLI
+  // final case class Kleisli[F[_], -A, B](run: A => F[B]) { self =>
+  val parseKleisli: Kleisli[Option, String, Int] =
+  Kleisli(parse)
+
+  val reciprocalKleisli: Kleisli[Option, Int, Double] =
+    Kleisli(reciprocal)
+
+
+  val x1 = reciprocalKleisli(2)
+  println(x1)
+  val parseAndReciprocalKleisli: Kleisli[Option, String, Double] = parseKleisli andThen reciprocalKleisli
+  val x2 = parseAndReciprocalKleisli("4")
+  println(x2)
+}
