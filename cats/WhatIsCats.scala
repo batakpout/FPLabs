c3f12a77f1377539fd511acdefec14f3ca1c6414
aamir <16462830+batakpout@users.noreply.github.com>
2024-03-06 19:51:25 +0530

    Create WhatIsCats.scala

---
+++ cats/WhatIsCats.scala
@@ -0,0 +1,11 @@
+/**
+  - Cats is a library which provides abstractions for FP in Scala.
+  
+  - A broader goal of Cats is provide a foundation for an ecosystem of pure, typelevel libraries to support FP in scala.
+  
+  - Monads, and related concepts, are the FP equivalent of OO design pattern- architectural building blocks that
+    turn up over and over again in code. They differ from OO pattern in 2 main ways:
+    1. they are formally, and thus precisely defined 2. they are extremely general.
+    Everyone find generality/abstraction difficult, however, it is generality that allows concepts like monads to be
+    applied in such a wide variety of situations.
+ */
